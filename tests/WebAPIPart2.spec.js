37cf6f91aa8299899e67a589669612ae274e68bd
kashyap013 <kashyapparikh1311@gmail.com>
2023-11-04 17:07:13 -0400

    Adding WebAPIPart2 file

--- tests/WebAPIPart2.spec.js
+++ tests/WebAPIPart2.spec.js
@@ -1,15 +1,35 @@
 const {test, expect} = require('@playwright/test');
 
-test('Practice on UI automation', async({page})=>
-{
-    await page.goto("https://rahulshettyacademy.com/client/")
-    console.log(await page.title());
-    
+const user_name = 'kyp1395@gmail.com';
+const user_password = 'Test@1234';
+let webContext;
+
+test.beforeAll(async({browser})=>{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/client/") 
 
     const logInTitle = page.locator("[class='login-title']");
     const userName = page.locator("#userEmail");
     const password = page.locator("#userPassword");
     const logInBtn = page.locator("#login");
+    const productTitle = page.locator(".card-body b");    
+
+    //Login code
+    await userName.fill(user_name);
+    await password.fill(user_password);
+    await logInBtn.click();
+    await productTitle.first().waitFor();
+    await context.storageState({path: 'state.json'});
+    webContext = await browser.newContext({storageState: 'state.json'});
+
+});
+
+test('Test case 1: Practice on UI automation', async()=>
+{
+    const page = await webContext.newPage();
+    await page.goto("https://rahulshettyacademy.com/client/") 
+
     const products = page.locator(".card-body");
     const productTitle = page.locator(".card-body b");
     const productName = "iphone 13 pro";
@@ -26,23 +46,11 @@ test('Practice on UI automation', async({page})=>
     const orderRows = page.locator("tbody tr");
     const summarayOrderID = page.locator(".col-text");
     
-
-    const user_name = 'kyp1395@gmail.com';
-    const user_password = 'Test@1234';
-    
-
-    //Login code
-    console.log(await logInTitle.textContent());
-    await expect(logInTitle).toContainText('Log in');
-    await userName.fill(user_name);
-    await password.fill(user_password);
-    await logInBtn.click();
-    await productTitle.first().waitFor();
-    
+    await productTitle.first().waitFor();    
     //console.log("First result title is: "+ await resultTitle.first().textContent());
     //await page.waitForLoadState("networkidle");
+   
     //List all available product titles
-    
     const allTitles = await productTitle.allTextContents();
     // console.log("Total number of available results are: " + allTitles.length);
     console.log(allTitles);
@@ -90,14 +98,6 @@ test('Practice on UI automation', async({page})=>
     await page.locator("button:has-text('Apply Coupon')").click();
 
     await couponAppliedText.waitFor();
-    //const couponMessage = await couponAppliedText.textContent(); 
-    // if(couponMessage === "* Coupon Applied"){
-    //     console.log("Coupon Status: " + couponMessage);
-    //     couponBool = true;
-    // }else if(couponMessage === "* Invalid Coupon"){
-    //     console.log("Coupon Status: " + couponMessage);
-    //     couponBool = false;
-    // }
     await expect(couponAppliedText).toHaveText("* Coupon Applied");
 
 
@@ -128,6 +128,20 @@ test('Practice on UI automation', async({page})=>
     expect(summarayOrderID).toHaveText(orderID);
     //await page.pause();
 
+});
+
+test('Test case 2: Login and print all available product titles', async()=>
+{
+    const page = await webContext.newPage();
+    await page.goto("https://rahulshettyacademy.com/client/") 
+
+    const productTitle = page.locator(".card-body b"); 
+    await productTitle.first().waitFor();
+    //List all available product titles
+    const allTitles = await productTitle.allTextContents();
+    // console.log("Total number of available results are: " + allTitles.length);
+    console.log(allTitles);
+    
 
 
 });
