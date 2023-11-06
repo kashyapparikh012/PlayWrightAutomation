8a0d87b935152f1a6ad64678a96cb983dfd0f0d0
kashyap013 <kashyapparikh1311@gmail.com>
2023-11-06 13:33:01 -0500

    Added code to take screenshot and compare screenshots

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -26,3 +26,22 @@ test('Popup validations', async({page})=>
     console.log(textCheck.split(" ")[1]);
     
 });
+
+test('Taking Screenshot', async({page})=>
+{
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#displayed-text").screenshot({path: 'partialScreenshot.png'});
+    await page.locator("#hide-textbox").click();
+    await page.screenshot({path: 'screenshot.png'});
+    await expect(page.locator("#displayed-text")).toBeHidden();
+    
+       
+});
+
+test.only('Visual Comparison of screenshots', async({page})=>
+{
+    await page.goto("https://www.google.com/");
+    expect(await page.screenshot()).toMatchSnapshot('landing.png');
+
+});
